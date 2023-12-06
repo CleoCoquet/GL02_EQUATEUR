539dd35c87b7f60e7b8c2db820fb4c3b46d8eb10
CleoCoquet <cleo.coquet@gmail.com>
2023-12-06 13:34:08 +0100

    Vcard

--- Src/Commands/generate-vcard.js
+++ Src/Commands/generate-vcard.js
@@ -1,1 +1,38 @@
 //5
+function generateVCard(teacherInfo) {
+    // Créer le contenu vCard
+    const vCardData = `
+BEGIN:VCARD
+VERSION:3.0
+FN:${teacherInfo.fullName}
+ORG:${teacherInfo.organization || ''}
+TEL:${teacherInfo.phoneNumber || ''}
+EMAIL:${teacherInfo.email || ''}
+END:VCARD
+`;
+
+    // Convertir le contenu en Blob
+    const blob = new Blob([vCardData], { type: 'text/vcard;charset=utf-8' });
+
+    // Créer un lien pour télécharger le fichier vCard
+    const downloadLink = document.createElement('a');
+    downloadLink.href = window.URL.createObjectURL(blob);
+    downloadLink.setAttribute('download', 'teacher_vcard.vcf');
+    document.body.appendChild(downloadLink);
+
+    // Simuler un clic sur le lien pour déclencher le téléchargement
+    downloadLink.click();
+
+    // Nettoyer le lien après le téléchargement
+    document.body.removeChild(downloadLink);
+}
+
+// Exemple d'utilisation
+const teacherInfo = {
+    fullName: 'John Doe',
+    organization: 'School XYZ',
+    phoneNumber: '123456789',
+    email: 'john.doe@example.com',
+};
+
+generateVCard(teacherInfo);
