17b4edc46acacba8394397eaecdfe3977ca7407b
CleoCoquet <cleo.coquet@gmail.com>
2023-12-06 16:31:50 +0100

    vcard_v2

--- Src/Commands/generate-vcard.js
+++ Src/Commands/generate-vcard.js
@@ -1,29 +1,43 @@
 const readline = require('readline');
+const fs = require('fs');
+const path = require('path');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+const dataVCardDir = 'data_vcard';
+
+// Vérifier si le répertoire existe, sinon le créer
+if (!fs.existsSync(dataVCardDir)) {
+  fs.mkdirSync(dataVCardDir);
+}
+
 function generateVCard() {
   // Demander à l'utilisateur d'entrer ses informations
   rl.question("Entrez votre nom complet: ", (fullName) => {
-    rl.question("Entrez le nom de votre organisation (facultatif): ", (organization) => {
+    rl.question("Entrez votre adresse e-mail (facultatif): ", (email) => {
       rl.question("Entrez votre numéro de téléphone (facultatif): ", (phoneNumber) => {
-        rl.question("Entrez votre adresse e-mail (facultatif): ", (email) => {
+        rl.question("Entrez le nom de votre établissement et son adresse (facultatif): ", (organization) => {
           // Créer le contenu vCard
           const vCardData = `
 BEGIN:VCARD
 VERSION:3.0
 FN:${fullName}
-ORG:${organization || ''}
-TEL:${phoneNumber || ''}
 EMAIL:${email || ''}
+TEL:${phoneNumber || ''}
+ORG:${organization || ''}
 END:VCARD
 `;
 
           // Afficher la vCard générée
           console.log('vCard générée :', vCardData);
+
+          // Enregistrer la vCard dans un fichier .vcf dans le répertoire data_vcard
+          const fileName = path.join(dataVCardDir, `${fullName.replace(/\s+/g, '_')}.vcf`);
+          fs.writeFileSync(fileName, vCardData, 'utf-8');
+          console.log(`vCard enregistrée dans le fichier ${fileName}`);
 
           // Fermer l'interface readline
           rl.close();
@@ -35,4 +49,24 @@ END:VCARD
 
 // Appeler la fonction pour générer la vCard
 generateVCard();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
