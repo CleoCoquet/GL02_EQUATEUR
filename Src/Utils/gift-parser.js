16d16b8a078d1cfb9df22a6391440b614289f93d
randall <randallmencias@gmail.com>
2023-12-07 12:31:38 +0100

    Parser fixed, add short answer

--- Src/Utils/gift-parser.js
+++ Src/Utils/gift-parser.js
@@ -68,87 +68,96 @@ function parseGift(giftString) {
 function QuestionType(line, currentQuestion) {
     // Remove curly braces and spaces
     const cleanLine = line.replace(/[{}]/g, '');
+    if (cleanLine) {
 
-    //the answers are split by the special characters
-    let answers = cleanLine.split(/([~=#])/).filter(Boolean);  // Filter to remove empty strings
-    const joinedAnswers = [];
-    for (let i = 0; i < answers.length; i += 2) {
-        if (i + 1 < answers.length) {
-            joinedAnswers.push(answers[i] + answers[i + 1]);
-        } else {
-            joinedAnswers.push(answers[i]);
-        }
-    }
-    answers = joinedAnswers;
-    // Multiple choice, matching or , short
-    //revisar que # de feedback no este no moleste con el de numerical
-    if (answers.length >= 2 && (answers[0].includes("~") || answers[0].includes("=") || answers[0].includes("->"))) {
-
-        answers.forEach(option => {
-                //agregar short answer
-                const match = option.split("#");
-                if (match) {
-                    if (match[0].includes("~") && !match[0].includes("->")) {
-                        currentQuestion.type = "MC";
-                        currentQuestion.choices.push(match[0]);
-                        currentQuestion.feedback.push(match[1]);
-                    } else if (match[0].includes("=") && !match[0].includes("->")) {
-                        currentQuestion.type = "MC";
-                        currentQuestion.choices.unshift(match[0]);
-                        currentQuestion.feedback.unshift(match[1]);
-                    } else {
-                        currentQuestion.type = "Matching";
-                        currentQuestion.choices.push(match[0]);
-                        currentQuestion.feedback.push(match[1]);
-                    }
-                } else {
-                    if (option.includes("~") && !option.includes("->")) {
-                        currentQuestion.type = "MC";
-                        currentQuestion.choices.push(option);
-                    } else if (option.includes("=") && !option.includes("->")) {
-                        currentQuestion.type = "MC";
-                        currentQuestion.choices.unshift(option);
-                    } else {
-                        currentQuestion.type = "Matching";
-                        currentQuestion.choices.push(option);
-                    }
-                }
-
-            }
-        );
-    } else {
-        const match = answers[0].split("#");
-
-        //Truth or false, short answer or essay, numerical
-        if (match.length === 2) {
-            if (answers[0] === "Truth" || answers[0] === "False" || answers[0] === "True"
-                || answers[0] === "F" || answers[0] === "T" || answers[0] === "TRUE" || answers[0] === "FALSE") {
-                currentQuestion.type = "TF";
-                currentQuestion.choices.push(answers[0]);
-                currentQuestion.feedback.push(match[1]);
-            } else if (answers[0] === "") {
-                currentQuestion.type = "Essay";
-                currentQuestion.choices.push(answers[0]);
-            }
-        } else {
-            if (answers[0].includes("#")) {
-                currentQuestion.type = "Numerical";
-                currentQuestion.choices.push(answers[0]);
-            } else if (answers[0] === "Truth" || answers[0] === "False" || answers[0] === "True"
-                || answers[0] === "F" || answers[0] === "T" || answers[0] === "TRUE" || answers[0] === "FALSE") {
-                currentQuestion.type = "TF";
-                currentQuestion.choices.push(answers[0]);
-                currentQuestion.feedback.push(match[1]);
-            } else if (answers[0] === "") {
-                currentQuestion.type = "Essay";
-                currentQuestion.choices.push(answers[0]);
-
+        console.log("cleanLine: " + cleanLine);
+        //the answers are split by the special characters
+        let answers = cleanLine.split(/([~=#])/).filter(Boolean);  // Filter to remove empty strings
+        const joinedAnswers = [];
+        for (let i = 0; i < answers.length; i += 2) {
+            if (i + 1 < answers.length) {
+                joinedAnswers.push(answers[i] + answers[i + 1]);
             } else {
-                currentQuestion.type = "Incomplete";
-                currentQuestion.choices.push(answers[0]);
-                currentQuestion.feedback.push("Incomplete Question");
+                joinedAnswers.push(answers[i]);
             }
         }
+        answers = joinedAnswers;
+        // Multiple choice, matching or , short
+        //revisar que # de feedback no este no moleste con el de numerical
+        if (answers.length >= 2 && (answers[0].includes("~") || answers[0].includes("=") || answers[0].includes("->"))) {
+
+            answers.forEach(option => {
+                    //agregar short answer
+                    answers[0] = answers[0].trim();
+                    const match = option.split("#");
+                    if (match) {
+                        if (match[0].includes("~") && !match[0].includes("->")) {
+                            currentQuestion.type = "MC";
+                            currentQuestion.choices.push(match[0]);
+                            currentQuestion.feedback.push(match[1]);
+                        } else if (match[0].includes("=") && !match[0].includes("->")) {
+                            currentQuestion.type = "MC";
+                            currentQuestion.choices.unshift(match[0]);
+                            currentQuestion.feedback.unshift(match[1]);
+                        } else {
+                            currentQuestion.type = "Matching";
+                            currentQuestion.choices.push(match[0]);
+                            currentQuestion.feedback.push(match[1]);
+                        }
+                    } else {
+                        if (option.includes("~") && !option.includes("->")) {
+                            currentQuestion.type = "MC";
+                            currentQuestion.choices.push(option);
+                        } else if (option.includes("=") && !option.includes("->")) {
+                            currentQuestion.type = "MC";
+                            currentQuestion.choices.unshift(option);
+                        } else {
+                            currentQuestion.type = "Matching";
+                            currentQuestion.choices.push(option);
+                        }
+                    }
+
+                }
+            );
+        } else {
+            console.log("answers: " + answers[0]);
+            const match = answers[0].split("#");
+
+            //Truth or false, short answer or essay, numerical
+            if (match.length === 2) {
+                if (answers[0] === "Truth" || answers[0] === "False" || answers[0] === "True"
+                    || answers[0] === "F" || answers[0] === "T" || answers[0] === "TRUE" || answers[0] === "FALSE") {
+                    currentQuestion.type = "TF";
+                    currentQuestion.choices.push(answers[0]);
+                    currentQuestion.feedback.push(match[1]);
+                } else if (answers[0] === "") {
+                    currentQuestion.type = "Essay";
+                    currentQuestion.choices.push(answers[0]);
+                }
+            } else {
+                if (answers[0].includes("#")) {
+                    currentQuestion.type = "Numerical";
+                    currentQuestion.choices.push(answers[0]);
+                } else if (answers[0] === "Truth" || answers[0] === "False" || answers[0] === "True"
+                    || answers[0] === "F" || answers[0] === "T" || answers[0] === "TRUE" || answers[0] === "FALSE") {
+                    currentQuestion.type = "TF";
+                    currentQuestion.choices.push(answers[0]);
+                    currentQuestion.feedback.push(match[1]);
+                } else if (answers[0] === "") {
+                    currentQuestion.type = "Essay";
+                    currentQuestion.choices.push(answers[0]);
+
+                } else {
+                    currentQuestion.type = "Incomplete";
+                    currentQuestion.choices.push(answers[0]);
+                    currentQuestion.feedback.push("Incomplete Question");
+                }
+            }
+        }
+    } else {
+        currentQuestion.type = "Incomplete";
+        currentQuestion.choices.push(cleanLine);
+        currentQuestion.feedback.push("Incomplete Question");
     }
 }
 
