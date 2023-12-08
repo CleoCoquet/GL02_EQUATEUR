01f0c7b4687381183748fa942bd0bcd176dc8e39
randall <randallmencias@gmail.com>
2023-12-08 14:48:46 +0100

    Main in caporal

--- Src/Commands/search-question.js
+++ Src/Commands/search-question.js
@@ -1,8 +1,4 @@
 const {Prompt} = require('./main.js');
 const {Questions} = require('./parsed-questions.js');
-
-Questions.forEach(question =>{
-    if(question.name.includes(Prompt) || question.text.includes(Prompt)){
-        console.log(question)}
-        module.exports = {question};
-    });
+const matchingQuestions = Questions.filter(question => question.includes(Prompt));
+module.exports = {matchingQuestions};
