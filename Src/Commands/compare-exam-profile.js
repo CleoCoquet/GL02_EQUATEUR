80d30e05e9b1e7408280f6a49a04a92f159e8244
docasdiva <dodoviadrake12@gmail.com>
2023-12-10 21:02:56 +0100

    createexam

--- Src/Commands/compare-exam-profile.js
+++ Src/Commands/compare-exam-profile.js
@@ -1,1 +1,44 @@
 //7
+
+Examen.prototype.compare = function (exam1, exam2, logger) {
+
+    fs.readFile('./profil-gift-exam/' + exam1 + '.gift', 'utf8', function (err, data) {
+        if (err) {
+            return logger.warn(err);
+        }
+
+        analyzer = new GiftParser();
+        analyzer.parse(data);
+
+        if (analyzer.errorCount === 0) {
+            //Ajout d'une ligne "exam" dans le tableau, utile pour générer l'histogramme souhaité
+            var data1 = analyzer.parsedQuestion.map(p => {
+                p["exam"] = exam1;
+                return p;
+            })
+        }
+
+
+        fs.readFile('./profil-gift-exam/' + exam2 + '.gift', 'utf8', function (err, data) {
+            if (err) {
+                return logger.warn(err);
+            }
+
+            analyzer2 = new GiftParser();
+            analyzer2.parse(data);
+
+            if (analyzer2.errorCount === 0) {
+                //Ajout d'une ligne "exam" dans le tableau, utile pour générer l'histogramme souhaité
+                var data2 = analyzer2.parsedQuestion.map(p => {
+                    p["exam"] = exam2;
+                    return p;
+                })
+            }
+
+        }
+        
+    
+    );
+    }
+    );
+}
