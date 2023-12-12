34c564cdaae4b59c482668792065a5bb52a09ace
CleoCoquet <150054288+CleoCoquet@users.noreply.github.com>
2023-12-12 16:06:22 +0100

    Update prepare_exam.js

--- Src/prepare_exam.js
+++ Src/prepare_exam.js
@@ -4,7 +4,7 @@ const giftparse = require('./gift-parser.js');
 const rechercher = require("./search-question.js");
 
 
-
+const dataquestion = 'data_question';
 
 const createExam = function (){
         console.log("creation d'un examen");
@@ -22,6 +22,9 @@ const createExam = function (){
           do{
             questionsExam.push(resultats);
             console.log("La question a été ajoutée \n");
+                  const fileName = path.join(dataquestion, `${question.replace(/\s+/g, '_')}.gift`);
+          fs.writeFileSync(fileName, questionData, 'utf-8');
+         
           }
           while 
         (insert.toLowerCase() === "oui");
